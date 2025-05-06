0f31ddb8200ace85b8b44347a11bc728dce3bc0a
Your Name <you@example.com>
2025-05-06 00:03:59 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -28,27 +28,83 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+final List<Widget> esqueceuSenhaButtom = <Widget>[
+  TextButton(
+    style: TextButton.styleFrom(overlayColor: Colors.blue),
+    onPressed: () {},
+    child: const Text('Esqueceu a senha?'),
+  ),
+];
+
 class _MyHomePageState extends State<MyHomePage> {
+  bool? isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.onPrimaryFixed,
         toolbarHeight: 250,
-        title: Image.asset('sleepk-team.png', fit: BoxFit.cover),
+        title: Image.asset('web/assets/sleepk-team.png', fit: BoxFit.cover,),
         centerTitle: true,
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(20.0),
+          child: Divider(height: 1, thickness: 3, color: Colors.red),
+        ),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           children: [
-            Padding(padding: EdgeInsets.fromLTRB(0, 100, 0, 0),
+        const Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
             child: SizedBox(
               width: 250,
               child: TextField(
                 obscureText: true,
-                decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Password'),
+                decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'E-mail'),
               ),
-            ))
+            )),
+            const Padding(padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: SizedBox(
+                  width: 250,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Password'),
+                  ),
+                )),
+            Padding(padding: const EdgeInsets.fromLTRB(0, 5, 130,0),
+                child: Directionality(
+                  textDirection: TextDirection.ltr,
+                    child: Column(children: esqueceuSenhaButtom),
+                  )
+                ),
+            Align(
+                alignment: Alignment.center,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          tristate: true,
+                          value: isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              isChecked = value;
+                            });
+                          },
+                        ),
+                        const Text.rich(
+                          TextSpan(
+                              text: 'Lembrar a senha',
+                              style: TextStyle(color: Colors.purple)// default text style
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                )
+            )
+            ,
           ],
         ),
     ));
