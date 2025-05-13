90f61069c6df9dc36ab3a786abe423dece28d039
Your Name <you@example.com>
2025-05-13 00:39:12 -0300

    ajuste tabela jogo

--- lib/apps/jogos.dart
+++ lib/apps/jogos.dart
@@ -6,22 +6,39 @@ class JogosPage extends StatelessWidget {
   const JogosPage({super.key});
 
   @override
-  Widget build(BuildContext context) => Center(
-    child: Container(
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title:
+      const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('Lista de jogos', style: TextStyle(color: Colors.white, fontSize: 18)),
+          Padding(padding: EdgeInsets.only(right: 25),
+          child: Icon(Icons.add_outlined),)
+        ],
+      ),
+      backgroundColor: const Color(0xff576196),
+    ),
+    body: Container(
         color: Colors.white,
-        child: DataTable(columns: const [
-          DataColumn(label: Text('Jogo')),
-          DataColumn(label: Text('Editar'))
-        ], rows: const [
-          DataRow(cells: [
-            DataCell(Text('teste')),
-            DataCell(Text('teste'))
-          ]),
-          DataRow(cells: [
-            DataCell(Text('teste')),
-            DataCell(Text('teste'))
-          ])
-        ])
+        child: ListView.separated(
+          itemCount: 2,
+          scrollDirection: Axis.vertical,
+          separatorBuilder: (BuildContext context, int index) => const Divider(color: Colors.grey,),
+          itemBuilder: (BuildContext context, int index) {
+            return const ListTile(
+              leading: CircleAvatar(
+                radius: 15,
+                backgroundImage: NetworkImage(
+                    'web/assets/ghost-golfing.jpg'),
+              ),
+              title: Text('teste                                   5.0', style: TextStyle(fontSize: 20),),
+              trailing: Icon(Icons.edit),
+
+            );
+          },
+          padding: const EdgeInsets.all(16),
+        )
     )
     );
 }
